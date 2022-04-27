77cc24ad7681ed04304ea7cca992329ad2a916ec
Ahmed Salah <ahmed_abdelsamie@outlook.com>
2022-04-27 22:43:15 +0200

    made the solution db

--- problem5.java
+++ problem5.java
@@ -1,21 +1,29 @@
 import java.io.*;
 import java.util.*;
 
-public class pb{
+public class problem5{
     static Scanner in = new Scanner(System.in);
     static PrintWriter out = new PrintWriter(System.out);
+
+
+    static final int MAXN = 100;
+    static long[] mem = new long[MAXN];
 
     public static void main(String[] args){
         System.out.print("Enter the value of n: ");
         int n = in.nextInt();
 
         System.out.println();
-        int answer = solve(n);
+        long answer = solve(n);
         System.out.println("The minimum number of moves is: "+answer);
     }
 
-    public static int solve(int n){
+    public static long solve(int n){
         if(n<=2) return n;
-        return 1 + 2*solve(n-2) + solve(n-1);
+        if(mem[n] != 0)
+            return mem[n];
+
+        mem[n] = 1 + 2*solve(n-2) + solve(n-1);
+        return mem[n];
     }
 }
